d443bcc602eb61aa971e82e462204ae3ac57d1c4
jrbtaylor <jasonrbtaylor@gmail.com>
2017-11-04 17:10:02 -0400

    initial commit: data loader for sequential/permuted MNIST

---
+++ data.py
@@ -0,0 +1,34 @@
+"""
+Written by Jason Taylor <jasonrbtaylor@gmail.com> 2017-2018
+"""
+
+import torch
+from torchvision import datasets, transforms
+
+dtype = torch.cuda.FloatTensor
+
+pixel_permutation = torch.randperm(28*28)
+
+def mnist_loaders(batch_size,sequential=True,permuted=True, n_workers=4):
+    if permuted:  # can't be permuted if not sequential
+        assert sequential
+
+    kwargs = {'num_workers': n_workers, 'pin_memory': True}
+    if not sequential:
+        transform = transforms.ToTensor()
+    elif not permuted:
+        transform = transforms.Compose([
+            transforms.ToTensor(),transforms.Lambda(lambda x: x.view(-1, 1))])
+    else:
+        transform = transforms.Compose([
+            transforms.ToTensor(),transforms.Lambda(
+                lambda x: x.view(-1, 1)[pixel_permutation])])
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('/media/jason/DataSSD1/data/mnist',
+                       train=True, download=True, transform=transform),
+        batch_size=batch_size, shuffle=True, **kwargs)
+    val_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('/media/jason/DataSSD1/data/mnist',
+                       train=False, download=True, transform=transform),
+        batch_size=batch_size, shuffle=True, **kwargs)
+    return train_loader,val_loader
