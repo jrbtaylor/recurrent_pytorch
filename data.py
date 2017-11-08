cbc5548784886c52dceb8df7e56c6a37559e6378
jrbtaylor <jasonrbtaylor@gmail.com>
2017-11-08 18:24:32 -0500

    change default data path

--- data.py
+++ data.py
@@ -24,11 +24,11 @@ def mnist(batch_size,sequential=True,permuted=True,n_workers=4):
             transforms.ToTensor(),transforms.Lambda(
                 lambda x: x.view(-1, 1)[pixel_permutation])])
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('/media/jason/DataSSD1/data/mnist',
+        datasets.MNIST('/home/jason/data/mnist',
                        train=True, download=True, transform=transform),
         batch_size=batch_size, shuffle=True, **kwargs)
     val_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('/media/jason/DataSSD1/data/mnist',
+        datasets.MNIST('/home/jason/data/mnist',
                        train=False, download=True, transform=transform),
         batch_size=batch_size, shuffle=True, **kwargs)
     return train_loader,val_loader
